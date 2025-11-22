5a61209b0c6c022a05ce3285cb847c5d2f5a7a85
yangelia <yana.sychova@gmail.com>
2025-11-23 01:40:53 +0200

    upd sign in\

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { api } from "../../api";
+import { cookies } from "next/headers";
 import { parse } from "cookie";
 import { isAxiosError } from "axios";
 import { logErrorResponse } from "../../_utils/utils";
@@ -9,53 +10,37 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const apiRes = await api.post("auth/login", body);
 
+    const cookieStore = await cookies();
     const setCookie = apiRes.headers["set-cookie"];
 
     if (setCookie) {
       const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-      const response = NextResponse.json(apiRes.data, {
-        status: apiRes.status,
-      });
-
       for (const cookieStr of cookieArray) {
         const parsed = parse(cookieStr);
-
         const options = {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none" as const,
-          path: parsed.Path || "/",
           expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-          maxAge: parsed["Max-Age"] ? Number(parsed["Max-Age"]) : undefined,
+          path: parsed.Path,
+          maxAge: Number(parsed["Max-Age"]),
         };
-
-        if (parsed.accessToken) {
-          response.cookies.set("accessToken", parsed.accessToken, options);
-        }
-        if (parsed.refreshToken) {
-          response.cookies.set("refreshToken", parsed.refreshToken, options);
-        }
+        if (parsed.accessToken)
+          cookieStore.set("accessToken", parsed.accessToken, options);
+        if (parsed.refreshToken)
+          cookieStore.set("refreshToken", parsed.refreshToken, options);
       }
 
-      return response;
+      return NextResponse.json(apiRes.data, { status: apiRes.status });
     }
 
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  } catch (error: unknown) {
+  } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
       return NextResponse.json(
-        {
-          error: error.message,
-          response: error.response?.data,
-        },
-        { status: error.response?.status ?? 500 }
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
       );
     }
-
-    const err = error as Error;
-    logErrorResponse({ message: err.message });
-
+    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
