c308a673fd07655e657c9e7327e5a06d1b3813dd
yangelia <yana.sychova@gmail.com>
2025-11-23 14:29:38 +0200

    upd api 2

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+export const api = axios.create({
+  baseURL: "https://notehub-api.goit.study",
   withCredentials: true,
 });
